b7afec1f4572b4fde827bf1ca8ee5f62d81e342a
J. Smeets <16883019+juulSme@users.noreply.github.com>
2025-12-16 19:07:06 +0100

    Require Elixir 1.15

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule SpeckEx.MixProject do
     [
       app: :speck_ex,
       version: "0.1.0",
-      elixir: "~> 1.19",
+      elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       description: """
