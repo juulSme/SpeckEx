39d03c1f09559020af361669fdd94b532955d0eb
J. Smeets <16883019+juulSme@users.noreply.github.com>
2025-12-16 19:01:58 +0100

    Add metadata and hex docs

--- mix.exs
+++ mix.exs
@@ -7,7 +7,23 @@ defmodule SpeckEx.MixProject do
       version: "0.1.0",
       elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: """
+      Speck block cipher for Elixir, backed by Rust crate "speck-cipher".
+      """,
+      package: [
+        licenses: ["Apache-2.0"],
+        links: %{github: "https://github.com/juulSme/SpeckEx"},
+        source_url: "https://github.com/juulSme/SpeckEx"
+      ],
+      source_url: "https://github.com/juulSme/SpeckEx",
+      name: "SpeckEx",
+      docs: [
+        source_ref: ~s(main),
+        extras: ~w(./README.md ./LICENSE.md),
+        main: "SpeckEx",
+        skip_undefined_reference_warnings_on: ~w()
+      ]
     ]
   end
 
@@ -24,6 +40,7 @@ defmodule SpeckEx.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
       {:rustler, "0.37.1"},
+      {:ex_doc, "~> 0.36", only: [:dev, :test], runtime: false},
       {:benchmark, github: "juulSme/benchmark_ex", only: [:dev, :test]}
     ]
   end
