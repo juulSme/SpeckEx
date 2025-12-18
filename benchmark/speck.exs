4382d903a76ba63295347dbc850be24169bf690f
J. Smeets <16883019+juulSme@users.noreply.github.com>
2025-12-18 17:56:14 +0100

    Add blowfish to benchmark

--- benchmark/speck.exs
+++ benchmark/speck.exs
@@ -1,11 +1,16 @@
 alias SpeckEx.Block
 
+tasks = 1
+
+key128 = :crypto.strong_rand_bytes(16)
+blowfish_e = :crypto.crypto_init(:blowfish_ecb, key128, true)
+blowfish_d = :crypto.crypto_init(:blowfish_ecb, key128, false)
 key256 = :crypto.strong_rand_bytes(32)
 aes128_256_e = :crypto.crypto_init(:aes_256_ecb, key256, true)
 aes128_256_d = :crypto.crypto_init(:aes_256_ecb, key256, false)
 speck128_256 = key256 |> Block.speck128_256_init!()
 speck96_144 = :crypto.strong_rand_bytes(18) |> Block.speck96_144_init!()
-speck64_128 = :crypto.strong_rand_bytes(16) |> Block.speck64_128_init!()
+speck64_128 = key128 |> Block.speck64_128_init!()
 
 block128 = :crypto.strong_rand_bytes(16)
 block96 = :crypto.strong_rand_bytes(12)
@@ -18,37 +23,45 @@ iv = :crypto.strong_rand_bytes(16)
 %{
   "Speck 64/128 block enc" => [
     fn -> Block.speck64_128_encrypt!(block64, speck64_128) end,
-    tasks: 1
+    tasks: tasks
   ],
   "Speck 96/144 block enc" => [
     fn -> Block.speck96_144_encrypt!(block96, speck96_144) end,
-    tasks: 1
+    tasks: tasks
   ],
   "Speck 128/256 block enc" => [
     fn -> Block.speck128_256_encrypt!(block128, speck128_256) end,
-    tasks: 1
+    tasks: tasks
   ],
-  "AES 128/256 block enc" => [fn -> :crypto.crypto_update(aes128_256_e, block128) end, tasks: 1],
+  "AES 128/256 block enc" => [
+    fn -> :crypto.crypto_update(aes128_256_e, block128) end,
+    tasks: tasks
+  ],
+  "Blowfish block enc" => [fn -> :crypto.crypto_update(blowfish_e, block128) end, tasks: tasks],
   "Speck 64/128 block dec" => [
     fn -> Block.speck64_128_decrypt!(block64, speck64_128) end,
-    tasks: 1
+    tasks: tasks
   ],
   "Speck 96/144 block dec" => [
     fn -> Block.speck96_144_decrypt!(block96, speck96_144) end,
-    tasks: 1
+    tasks: tasks
   ],
   "Speck 128/256 block dec" => [
     fn -> Block.speck128_256_decrypt!(block128, speck128_256) end,
-    tasks: 1
+    tasks: tasks
   ],
-  "AES 128/256 block dec" => [fn -> :crypto.crypto_update(aes128_256_d, block128) end, tasks: 1],
+  "AES 128/256 block dec" => [
+    fn -> :crypto.crypto_update(aes128_256_d, block128) end,
+    tasks: tasks
+  ],
+  "Blowfish block dec" => [fn -> :crypto.crypto_update(blowfish_d, block128) end, tasks: tasks],
   "AES 128/256 CTR enc" => [
     fn -> :crypto.crypto_one_time(:aes_256_ctr, key256, iv, plaintext, true) end,
-    tasks: 1
+    tasks: tasks
   ],
   "Speck 128/256 CTR enc" => [
     fn -> SpeckEx.encrypt(plaintext, key256, iv) end,
-    tasks: 1
+    tasks: tasks
   ]
 }
 |> Benchmark.bench_many()
